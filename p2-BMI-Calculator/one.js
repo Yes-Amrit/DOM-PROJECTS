73fe445d69f87a1219418c72032b37785781a210
Amrit Raj <143371086+Yes-Amrit@users.noreply.github.com>
2025-06-14 08:49:26 +0000

    second project Done

--- p2-BMI-Calculator/one.js
+++ p2-BMI-Calculator/one.js
@@ -1,40 +1,3 @@
-# DOM-PROJECTS
-
-## Plenty of DOM project for beginners to practice
-
-### [Repo Link](https://github.com/Yes-Amrit/DOM-PROJECTS)
-
-### Project - 1
-#### Color Changing on Click
-``` javascript
-const buttons = document.querySelectorAll('.button')
-const body = document.querySelector('body')
-
-buttons.forEach(function(button){
-  console.log(button)
-  button.addEventListener('click', function(e){
-    console.log(e);
-    console.log(e.target);
-    if(e.target.id==='grey'){
-      body.style.backgroundColor = e.target.id;
-    }
-    if(e.target.id==='white'){
-      body.style.backgroundColor = e.target.id;
-    }
-    if(e.target.id==='blue'){
-      body.style.backgroundColor = e.target.id;
-    }
-    if(e.target.id==='yellow'){
-      body.style.backgroundColor = e.target.id;
-    }
-  })
-})
-```
-
-### Project - 2
-#### BMI Calculator
-
-``` Javascript
 // const height = document.querySelector('.height')             //this usecase will give empty value, so better take it inside form
 // const weight = document.querySelector('.weight')
 
@@ -42,6 +5,8 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', function(e){
     e.preventDefault()            //this will prevent it from sending or posting any link b/c we dont want the submit button to send any value
+
+
     const height = parseInt(document.querySelector('#height').value);       // .value se hume value mil jati h, aur wo string me milti h isiliye hum use int me change krenge 
     const weight = parseInt(document.querySelector('#weight').value);
     const results =document.querySelector('#results');
@@ -55,5 +20,5 @@ form.addEventListener('submit', function(e){
         const bmi =(weight/((height* height)/10000)).toFixed(2)
         results.innerHTML = `<span> ${bmi} </span> `
     }
+
 })
-```

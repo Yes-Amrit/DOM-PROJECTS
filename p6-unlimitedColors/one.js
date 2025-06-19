7cbdcabcd4da384a75dabb6cfc55ca55945cb2c6
Amrit Raj <143371086+Yes-Amrit@users.noreply.github.com>
2025-06-19 08:53:29 +0000

    Project - 6

--- p6-unlimitedColors/one.js
+++ p6-unlimitedColors/one.js
@@ -1,30 +1,27 @@
-//generate a random color
-
-const randomColor = function () {
-  const hex = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += hex[Math.floor(Math.random() * 16)];
+const randomColor = function(){
+  let hex = "0123456789ABCDEF"
+  let color = "#"
+  for(let i=0;i<6;i++){
+    color += hex[Math.floor(Math.random() * 16)]
   }
   return color;
 };
 
 let intervalId;
-const startChangingColor = function () {
-  if (!intervalId) {
-    intervalId = setInterval(changeBgColor, 1000);
-  }
 
-  function changeBgColor() {
+const startChangingColor = function(){
+  if(!intervalId){
+    intervalId = setInterval(changeColor, 1000);
+  }
+  function changeColor(){
     document.body.style.backgroundColor = randomColor();
   }
 };
-const stopChangingColor = function () {
+
+const stopChangingColor = function(){
   clearInterval(intervalId);
   intervalId = null;
 };
 
-document.querySelector('#start').addEventListener('click', startChangingColor);
-
-document.querySelector('#stop').addEventListener('click', stopChangingColor);
-
+document.querySelector("#start").addEventListener('click',startChangingColor);
+document.querySelector("#stop").addEventListener('click',stopChangingColor);
